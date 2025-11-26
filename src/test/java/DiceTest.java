974dad9c5605787d4c6ca2799b2f0d3f4d1ebd3b
amussa <ahmed.mussa@booking.com>
2025-11-26 13:44:58 +0000

    added GameTest and working on the edit on Test Dice

--- src/test/java/DiceTest.java
+++ src/test/java/DiceTest.java
@@ -1,20 +1,21 @@
-//import Dice;
-//import org.junit.Test;
 
-//import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-//public class DiceTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-    //@Test
-    //public void shouldReturnRandomNumberDice() {
+public class DiceTest {
+
+    @Test
+    public void shouldReturnRandomNumberDice() {
         //assemble
-        //Dice dice = new Dice();
+        Dice dice = new Dice();
 
         //act
-        //int result = Dice.dice(6);
+        int result = Dice.dice(6);
 
         //assert
-        //org.junit.Assert.assertTrue(result >= 1 && result <= 6);
-        //assertEquals(Dice.dice(1), (Math.random() * Dice.dice(1)) + 1);
-    //}
-//}
+        Assertions.assertTrue(result >= 1 && result <= 6);
+        assertEquals(Dice.dice(1), (Math.random() * Dice.dice(1)) + 1);
+    }
+}
---
+++ src/test/java/GameTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    public void ShouldReturnStringAsAnInputName() {
+        //assemble
+        Game MainStory = new Game();
+
+        //act
+
+
+        //assert
+        Assertions.assertTrue(true);
+
+    }
+
+
+}
+
